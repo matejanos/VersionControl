bf3c7511cb8dac03f04391487b52ab2f0829bb16
Mate Janos <janos.mate@bcecid.net>
2020-09-23 18:37:14 +0200

    kiiratas

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,9 +19,11 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+
             label1.Text = Resource1.FullName;
       
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.WriteToFile;
 
 
             listBox1.DataSource = users;
@@ -32,10 +35,27 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                FullName = label1.Text           
+                FullName = textBox1.Text         
 
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog()!= DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var s in users)
+                {
+                    sw.Write(s.ID);
+                    sw.Write(";");
+                    sw.Write(s.FullName);
+                    sw.WriteLine();
+                } 
+            }
+
         }
     }
 }
