fd6249efb7d3ac308cd5041408f5801acb5197a9
Mate Janos <janos.mate@bcecid.net>
2020-10-08 12:52:25 +0200

    PortfolioValueCalculation

--- QYE8OW_VaR/QYE8OW_VaR/Form1.cs
+++ QYE8OW_VaR/QYE8OW_VaR/Form1.cs
@@ -34,6 +34,20 @@ namespace QYE8OW_VaR
             dataGridView2.DataSource = Portfolio;
         }
 
-        
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
+
     }
 }
