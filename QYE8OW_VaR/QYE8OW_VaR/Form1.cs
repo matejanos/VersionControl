b6290c935c90a09b871b00cee90dd16396c68b6e
Mate Janos <janos.mate@bcecid.net>
2020-10-08 12:39:48 +0200

    PortfolioToMemory

--- QYE8OW_VaR/QYE8OW_VaR/Form1.cs
+++ QYE8OW_VaR/QYE8OW_VaR/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QYE8OW_VaR.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,13 +15,25 @@ namespace QYE8OW_VaR
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
-        
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
 
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
         }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+            dataGridView2.DataSource = Portfolio;
+        }
+
+        
     }
 }
