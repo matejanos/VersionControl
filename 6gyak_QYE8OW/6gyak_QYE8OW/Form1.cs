b8b1e304f54a906f7c61e2e043c108f1455c1c8d
Mate Janos <janos.mate@bcecid.net>
2020-10-18 11:13:50 +0200

    Webszolgáltatás hívása

--- 6gyak_QYE8OW/6gyak_QYE8OW/Form1.cs
+++ 6gyak_QYE8OW/6gyak_QYE8OW/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6gyak_QYE8OW.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,21 @@ namespace _6gyak_QYE8OW
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            
         }
     }
 }
