ed99d662dceb4ac3bc55dfbab1b8980b8cf4ce4e
Mate Janos <janos.mate@bcecid.net>
2020-10-25 10:25:42 +0100

    Szimulációs lépés

--- mikroszimulacio_QYE8OW/mikroszimulacio_QYE8OW/Form1.cs
+++ mikroszimulacio_QYE8OW/mikroszimulacio_QYE8OW/Form1.cs
@@ -30,7 +30,7 @@ namespace mikroszimulacio_QYE8OW
              
                 for (int i = 0; i < Population.Count; i++)
                 {
-                   
+
                 }
 
                 int nbrOfMales = (from x in Population
@@ -45,6 +45,35 @@ namespace mikroszimulacio_QYE8OW
 
         }
 
+        }
+
+        public void SimStep( int year, Person person)
+        {
+            if (!person.IsAlive) return;
+            byte age = (byte)(year - person.BirthYear);
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+
+         
         }
 
         public List<Person> GetPopulation(string csvpath)
@@ -110,5 +139,7 @@ namespace mikroszimulacio_QYE8OW
 
             return deathProbabilities;
         }
+
+      
     }
 }
