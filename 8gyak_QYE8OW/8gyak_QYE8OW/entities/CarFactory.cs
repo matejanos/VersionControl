f3f2dcd1a55661666318985c53a15dc2ede40939
Mate Janos <janos.mate@bcecid.net>
2020-11-11 15:13:08 +0100

    Absztrakció felhasználása a gyárakban

--- 8gyak_QYE8OW/8gyak_QYE8OW/entities/CarFactory.cs
+++ 8gyak_QYE8OW/8gyak_QYE8OW/entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace _8gyak_QYE8OW.entities
 {
-    public class BallFactory: IToyFactory
-    {
+    public class CarFactory: IToyFactory
+    {  
         public Toy CreatNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
