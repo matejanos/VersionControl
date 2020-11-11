11630671b7fed11be0d88515917154c586fc8dad
Mate Janos <janos.mate@bcecid.net>
2020-11-11 13:09:15 +0100

    Labdagyár létrehozása

---
+++ 8gyak_QYE8OW/8gyak_QYE8OW/entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8gyak_QYE8OW.entities
+{
+    public class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
