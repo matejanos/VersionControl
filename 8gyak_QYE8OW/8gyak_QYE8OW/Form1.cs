16b52438adefebc9efb3ab3be63cf4a3a957a309
Mate Janos <janos.mate@bcecid.net>
2020-11-11 17:15:38 +0100

    Termékválasztó gombok

--- 8gyak_QYE8OW/8gyak_QYE8OW/Form1.cs
+++ 8gyak_QYE8OW/8gyak_QYE8OW/Form1.cs
@@ -10,12 +10,26 @@ namespace _8gyak_QYE8OW
     public partial class Form1 : Form
     {
         private List<Toy> _toys = new List<Toy>();
-
+        private Toy _nextToy;
         private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { _factory = value;
+                DisplayNext();
+            }
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+
         }
 
         public Form1()
@@ -48,6 +62,16 @@ namespace _8gyak_QYE8OW
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
         }
     }
 }
